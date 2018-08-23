4d58f369a8c49d302ce62b15a26129a1b567d2b6
Julio Avila <javila3090@gmail.com>
2018-08-22 21:25:14 -0300

    working  on home page

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,14 +5,12 @@
           <a class="navbar-brand" href="{{url('/')}}">D-UÑAS & PIES</a>
           <h1 class="size-12px" style="letter-spacing: 1px; line-height: 1.5"></h1>
         </div>-->
-        <div class="col-md-4 col-xs-12 size-16px text-left">
+        <!--<div class="col-md-4 col-xs-12 size-16px text-left">
           <h3 class="size-18px">Formas de pago</h3>
           <img src="{{asset('img/webpay_lipocero.png')}}" class="img img-responsive" style="width: 70%">
-        </div>
+        </div>-->
         <div class="col-md-12 col-xs-12 size-16px">
-          <br/>
-          <br/>
-          <h3 class="size-12px">Copyright &copy; D-Uñas & Pies {{date('Y')}}</h3>
+          <h3 class="size-12px">Copyright &copy; Julio Avila {{date('Y')}}</h3>
           <div class="credits">            
             <!--
               All the links in the footer should remain intact.
